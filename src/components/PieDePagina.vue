d5913c09a430ab68d221885dfed0571675a5079f
Alvaro Oxley <alvarooxley@gmail.com>
2026-05-20 17:33:32 -0300

    dist

--- src/components/PieDePagina.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer class="newspaper-footer">
-    La opinión — El Gran Diario Argentino &nbsp;·&nbsp; Todos los derechos reservados · Simulación histórica
-  </footer>
-</template>
-
-<style scoped>
-/* Las clases están en style.css */
-</style>
